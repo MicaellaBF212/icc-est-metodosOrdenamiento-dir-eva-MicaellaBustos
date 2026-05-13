68e58902f569139deff39dadf4eca1f74b034814
Your Name <you@example.com>
2026-05-13 13:07:34 +0000

    Ya acabe

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
+import controllers.CarController;
 import models.Car;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CarController controller = new CarController();
+        Car[] carros = makeCarsList();
+        controller.sortByname(carros);
+        for (Car car: carros)
+            System.out.println(carros);
     }
 
     public static Car[] makeCarsList() {
---
+++ src/controllers/CarController.java
@@ -0,0 +1,6 @@
+package controllers;
+
+public class CarController {
+  public void sortByname(Car[] cars)
+  
+}
